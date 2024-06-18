b6d601873a0e84ad692888119db50483271bfdf8
Kali Cajala <kcajala@creativeelectron.com>
2024-06-18 10:38:08 -0700

    adding Word.py

---
+++ Word.py
@@ -0,0 +1,17 @@
+# used to determine how to translate a word 
+class Model:
+
+    # parameter
+    word = ""
+
+    # default constructor
+    def __init__(self, w):
+        # sets the word parameter
+        self.
+    
+    # default destructor
+    def __del__(self):
+        # nothing to implement, print statement for confirmation
+        print("Destructor called.")
+    
+    # 
