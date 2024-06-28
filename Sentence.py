dca28dfc4a3ddbc1c1bfbe64baeed3d48b8db3bd
Kali Cajala <kcajala@creativeelectron.com>
2024-06-28 09:03:53 -0700

    Updating Sentence file

--- Sentence.py
+++ Sentence.py
@@ -7,27 +7,12 @@ class Sentence:
     def __init__(self, s):
         self.sentence = s
 
-    # destructor
-    # translate fucntion
+    # translate function
     def translate_sentence(self):
         total_translation = ""
-        words_list = self.sentence.split()
+        words_list = self.sentence.split()   # makes a list of the words in the sentence(s) by splitting by every whitespace
         for w in words_list:
-            temp = Word(w)
-            total_translation += temp.translate_word()
-            total_translation += " "
-        return total_translation
-
-# def main():
-#     test = input("enter a word: ")
-#     s = Sentence(test)
-#     s.translate_sentence()
-
-
-# if __name__ == "__main__":
-#     main()
-
-# test = input("enter a sentence: ")
-# s = Sentence(test)
-# sentence = s.translate_sentence()
-# print(sentence)
+            temp = Word(w)   # calls to the Word class
+            total_translation += temp.translate_word()   # adds individual word translation to the total translation
+            total_translation += " "   # adds a space to separate the words
+        return total_translation   # returns the entire translation of the sentence(s)
